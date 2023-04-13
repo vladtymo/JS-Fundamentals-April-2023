7e52b9f9ef9e0d64a7f1d2a4f88506188f016566
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-04-13 20:45:55 +0300

    Operators example.

---
+++ 01 Intro/js/operators.js
@@ -0,0 +1,23 @@
+// ------------ operators ------------
+// ariphmetic operators
+
+console.log(`a + b: ${5 + 3}`); // 8
+console.log(`a - b: ${5 - 3}`); // 2
+console.log(`a * b: ${5 * 3}`); // 15
+console.log(`a / b: ${20 / 4}`); // 1.xxx
+console.log(`a % b: ${5 % 3}`); // 2
+
+// exponentiation **
+console.log(`a ** b: ${2 ** 8}`);
+
+// logic operators
+console.log(`a > b: ${5 > 3}`); // true
+console.log(`a < b: ${5 < 3}`); // false
+console.log(`a >= b: ${5 >= 3}`); // true
+console.log(`a <= b: ${20 <= 20}`); // true
+
+console.log(`a == b: ${'5' == 5}`); // true
+console.log(`a != b: ${5 != 3}`); // true
+// strict - compare types also
+console.log(`a === b: ${'5' === 5}`); // false
+console.log(`a !== b: ${5 !== 3}`); // false
