78c0dcc9951eca6e7c06614a9d12b3199f1c251c
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-05-02 20:54:05 +0300

    Create event handler dynamically.

--- 05_Forms/main.js
+++ 05_Forms/main.js
@@ -16,15 +16,25 @@ console.log("Price:", priceInput.value);
 console.log("In Stock:", inStock.checked);
 
 // ------------ set click event handler on submit button
+class Product {
+    static count = 1;
+
+    constructor(model, price, inStock) {
+        this.model = model;
+        this.price = price;
+        this.inStock = inStock;
+        this.id = Product.count++;
+    }
+
+    get htmlId() {
+        return `product-${this.id}`;
+    }
+}
+
 createForm.onsubmit = (event) => {
     event.preventDefault(); // disable default logic
 
-    const newProduct = {
-        id: 123,
-        model: modelInput.value,
-        price: priceInput.value,
-        inStock: inStock.checked
-    };
+    const newProduct = new Product(modelInput.value, priceInput.value, inStock.checked);
 
     addProductToTable(newProduct);
 }
@@ -33,10 +43,17 @@ createForm.onsubmit = (event) => {
 function addProductToTable(product) {
     //const tableBody = document.getElementById('product-table-body');
     // we can use element by ID as a variable
-    tableBody.innerHTML += `<tr>
-                                <th scope="row">${123}</th>
+    tableBody.innerHTML += `<tr id=${product.htmlId}>
+                                <th scope="row">${product.id}</th>
                                 <td>${product.model}</td>
                                 <td>${product.price}$</td>
                                 <td>${product.inStock ? 'In Stock' : 'Out of Stock'}</td>
+                                <td><button onclick="removeProduct('${product.htmlId}')" type="button" class="btn btn-danger">Remove</button></td>
                             </tr>`;
+}
+
+// remove product from table by ID
+function removeProduct(id) {
+    let item = document.getElementById(id);
+    item.remove();
 }
