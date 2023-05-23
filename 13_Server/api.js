42472e79a7e90f2a807bc8abbf51d5f6e2d91138
Vlad Tymoshchuk <59306514+vladtymo@users.noreply.github.com>
2023-05-23 21:13:45 +0300

    Create API using Express.

--- 13_Server/api.js
+++ 13_Server/api.js
@@ -1,3 +1,50 @@
-// express - application framework for building RESTful APIs with Node.js
+// server logic
 
-const app = require('express');
+const express = require('express'),
+    app = express();
+
+const hostname = 'localhost';
+const port = 3600;
+
+// create endpoint - method to handle client request
+
+// GET: ~/api/users
+app.get("/api/users?", (req, res) => {
+    res.status(200).type('text').send("List of users!");
+});
+// GET: ~/api/contacts
+app.get("/api/contacts", (req, res) => {
+    res.status(200).type('text').send("List of contacts!");
+});
+// POST: ~/api/users/vlad/tymo
+app.post("/api/users/:name/:surname", (req, res) => {
+    // req.params - get route parameters value
+
+    const name = req.params.name;
+    const surname = req.params.surname;
+    res.status(200).type('text').send(`User ${name} ${surname} was created! (route params)`);
+});
+app.post("/api/users", (req, res) => {
+    // req.query - get query parameters value
+
+    const name = req.query.name;
+    const surname = req.query.surname;
+    res.status(200).type('text').send(`User ${name} ${surname} was created! (query params)`);
+});
+app.get('/api/users/random', (req, res) => {
+    res.status(200).type('json');
+    res.json({
+        id: 1,
+        name: 'Bob',
+        surname: 'Arum',
+        age: 90
+    });
+});
+
+app.use((req, res, next) => {
+    res.status(404).type('text').send("Page Not Found!");
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server is listening http://${hostname}:${port}`);
+});
